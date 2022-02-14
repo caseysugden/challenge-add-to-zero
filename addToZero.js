f4be1d075e9f0eef1cf320c9e8cc4646f8f67916
caseysugden <66505022+caseysugden@users.noreply.github.com>
2022-02-14 10:33:06 -0700

    Solved addToZero

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,13 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+addToZero = true;
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i === j) { continue; }
+    if (array[i] + array[j] === 0) {
+      return console.log(`${addToZero} ${array[i]} ${array[j]}`);
+    }
+  }
+}
